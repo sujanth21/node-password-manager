ef66a4de0bc2a8007e4c8d29c4c7d4455fdab395
Sujanth Sebamalaithasan <sujanth@Sujanths-MacBook-Pro.local>
2017-05-24 23:20:55 +1000

    Created createAccount skeleton function

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ console.log('Starting Password Manager');
 var storage = require('node-persist');
 storage.initSync();
 
+//account.name Facebook
+//account.username User123!
+//account.password Password123!
+
+function createAccount(account) {
+    var accounts = storage.getItemSync('accounts');
+
+    //if accounts is undefined (use typeof)
+    // set accounts = []
+
+    //push on new account
+
+    //return account
+}
+
 
 
 
